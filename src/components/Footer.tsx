5778180b9e749266c3bf45bb2df7c13893995e43
sunyue <sunyue@knowhy.work>
2024-08-08 18:34:47 +0800

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,17 +2,23 @@ import Image from "next/image";
 
 const Footer = () => {
     return (
+
         <footer
-            className="z-50 w-full rounded-t-[40px] flex flex-col md:flex-row items-center justify-center bg-backgroundSecondary py-6 fixed bottom-0">
-            <div className="flex flex-col md:flex-row items-center justify-between w-full max-w-[1200px] px-4">
-                <div className="flex items-center mb-4 md:mb-0">
-                    <Image src="/images/logo_img.png" alt="Logo" width={44} height={37.29}
-                           className="mr-0 md:mr-[12px] md:w-[44px] md:h-[37.29px] w-[28px] h-[28px]"/>
-                    <div className="text-textPrimary font-hanalei ml-2 md:ml-4 text-sm md:text-[30px]">
+            className="w-full bg-backgroundSecondary py-[24px] rounded-t-[20px]
+                                                               mmd:rounded-t-[40px]">
+            <div className="mx-auto flex flex-col
+                                    mmd:flex-row items-center justify-between max-w-[1200px]">
+                <div className="flex items-center mb-[12px]
+                                                  mmd:mb-0">
+                    <Image src="/logo_img.png" alt="Logo" width={44} height={37.29}
+                           className="object-contain w-[28px]     h-[23px]
+                                                     mmd:w-[44px] mmd:h-[37.29px]"/>
+                    <div className="text-textPrimary font-hanalei text-[20px]     ml-[4px]
+                                                                  mmd:text-[30px] mmd:ml-[12px]">
                         TAPTAP
                     </div>
                 </div>
-                <div className="text-textPrimary text-[14px]">
+                <div className="text-textPrimary text-[14px] font-montserrat">
                     © 2024 TAPTAP.meme
                 </div>
             </div>
