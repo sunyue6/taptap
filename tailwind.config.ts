c8be7164be484a769860b6e82d6054ea9d235186
sunyue <sunyue@knowhy.work>
2024-08-09 09:05:22 +0800

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,7 +63,7 @@ const config: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
             "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      }
     },
   },
   plugins: [],
