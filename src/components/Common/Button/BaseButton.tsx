643b0083963960077b19e011914a8180d71211fc
sunyue <sunyue@knowhy.work>
2024-08-09 09:05:50 +0800

    update

--- src/components/Common/Button/BaseButton.tsx
+++ src/components/Common/Button/BaseButton.tsx
@@ -7,7 +7,6 @@ interface BaseButtonProps {
 
 const BaseButton: React.FC<BaseButtonProps> = ({ text = "BaseButton", onClick }) => {
     return (
-        // 移动
         <button
             className="bg-primary text-textPrimary px-4 py-2 rounded-[40px] hover:bg-primaryHover lg:ml-20 md:mr-0 mr-[12px] whitespace-nowrap text-[16px]"
             onClick={onClick}
