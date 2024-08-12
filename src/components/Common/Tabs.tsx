38fde27707c251e3c16a097cfaf081c0498b64ba
sunyue <sunyue@knowhy.work>
2024-08-12 19:19:54 +0800

    update

--- src/components/Common/Tabs.tsx
+++ src/components/Common/Tabs.tsx
@@ -10,20 +10,20 @@ interface TabsProps {
     borderClass?: string; // Optional prop to customize the border class
 }
 
-const Tabs: React.FC<TabsProps> = ({ tabs,borderClass = 'border-b-[1px]' }) => {
+const Tabs: React.FC<TabsProps> = ({ tabs,borderClass =  'border-b-[1px]' }) => {
     const [activeTab, setActiveTab] = useState(0);
 
     return (
-        <div className="w-full  mx-auto font-montserrat">
-            <div className="flex">
+        <div className="w-full mx-auto font-montserrat">
+            <div className={`flex`}>
                 {tabs.map((tab, index) => (
                     <button
                         key={index}
                         className={`flex-1 py-2 text-[16px] font-medium transition-colors gap-[16px] ${
                             index === activeTab
-                                ? 'border-b-2 border-success text-success'
-                                : `${borderClass}  text-textSecondary hover:text-success`
-                        }`} // ${index < tabs.length - 1 ? '' : ''} Add margin-right to all but the last tab
+                                ? 'border-b-[2px] border-success text-success'
+                                : `${borderClass} text-textSecondary hover:text-success`
+                        }`} // ${borderClass}
                         onClick={() => setActiveTab(index)}
                     >
                         {tab.label}
